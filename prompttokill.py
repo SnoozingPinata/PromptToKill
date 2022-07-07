1aaa5413cd4351211f3bec2d2d43cf714d141244
smelton <s.melton@rocketmail.com>
2022-07-06 23:04:11 -0500

    continued work

--- prompttokill.py
+++ prompttokill.py
@@ -1,6 +1,6 @@
 
 import os
-import datetime
+import time
 import PySimpleGUI as sg
 
 # notes:
@@ -9,32 +9,30 @@ import PySimpleGUI as sg
 
 
 # format is: year, month, day, hour, minute, second, microsecond
-start_time = datetime.datetime.now()
-end_time = start_time + datetime.timedelta(minutes=10)
+start_time = int(round(time.time() * 100))
 
 
 def Cancel_program(executable_name):
     os.system(f"taskkill /f /im {executable_name}")
 
 
-sg.theme('BluePurple')
-
-layout = [
-            [sg.Text('ProContractor will be terminated in:')],
-            [sg.Text(key='-TIMER-')],
-            [sg.Button('Kill'), sg.Button('Cancel')]
-        ]
+layout = [ 
+    [sg.Text('ProContractor will be terminated in:')], 
+    [sg.Text("timer text", key='-TIMER-')], 
+    [sg.Button('Kill'), sg.Button('Cancel'), sg.Button('DoesNothing')]
+    ]
 
 window = sg.Window('Prompt2Kill', layout)
 
 while True:
     event, values = window.read()
-    print(event, values)
-    current_time = datetime.datetime.now()
-    window['-TIMER-'].update(end_time - current_time)
+    current_time = int(round(time.time() * 100)) - start_time
+    window['-TIMER-'].update("this text was updated")
     if event == sg.WIN_CLOSED or event == 'Cancel':
         break
-    if event == 'Kill' or current_time >= end_time:
+    if event == 'Kill':
         #Cancel_program("maxwell.stp.infrastructure.shell.exe")
         Cancel_program("chrome.exe")
         window.close()
+    window.refresh()
+    
